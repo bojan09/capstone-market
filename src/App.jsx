32e4f8b9d41f33b0943091addbbf7ce53490f2ec
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-01-09 21:30:27 +0100

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import "./App.css";
 import React from "react";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
