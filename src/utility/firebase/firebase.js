2ef1d63764350573e728cbcbce812d0431bd57b2
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-01-14 10:51:36 +0100

    setting up firebase auth provider

--- src/utility/firebase/firebase.js
+++ src/utility/firebase/firebase.js
@@ -1,4 +1,10 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDosNQXkAMJcxh99bNrZUeYKBW7Gcrse00",
@@ -11,3 +17,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
