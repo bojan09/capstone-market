f958748be482c899eefe4f61a05bb0a7f6a24fcc
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-02-07 19:23:58 +0100

    added Open/Close CartDropdown Menu logic

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,11 +1,18 @@
 import "./CartIcon.scss";
+import { useContext } from "react";
+
+// context
+import { CartContext } from "../../context/cart/CartContext";
 
 // svg
 import { ReactComponent as ShoppingCartIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingCartIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
