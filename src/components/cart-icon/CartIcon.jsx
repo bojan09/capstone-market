ed2250a09574f18418c952cfb6b4d9543bb864fd
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-02-08 18:44:47 +0100

    added CartDropdown count

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -10,11 +10,12 @@ import { ReactComponent as ShoppingCartIcon } from "../../assets/shopping-bag.sv
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const { cartCount } = useContext(CartContext);
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingCartIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
